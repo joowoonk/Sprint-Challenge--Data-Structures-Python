cf50cd75a303cb4ba3409f16ff652849ec77c179
Golden Eagle <joowoon.kang@biola.edu>
2020-06-12 09:02:58 -0700

    finished

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.newArray = []
+        self.savedCap = capacity
 
     def append(self, item):
-        pass
+        if self.capacity > 0:
+            # print("1")
+            self.newArray.append(item)
+            self.capacity -= 1
+        elif self.capacity == 0:
+            self.capacity = self.savedCap * -1
+            # print("2")
+            self.newArray[self.capacity] = item
+            self.capacity += 1
+        elif self.capacity < 0:
+            # print("3")
+            self.newArray[self.capacity] = item
+            self.capacity += 1
 
     def get(self):
-        pass
+        return self.newArray
